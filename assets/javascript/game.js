c324f6d356781dd21b6f73ef36fb0ea5ddf1e17a
Andrea Minhas <andreaminhas@Andreas-MacBook-Pro.local>
2018-11-08 21:37:56 -0600

    JS started

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+var letter = ["a", "e", "i", "o", "u"];
+    
+    var wins = 0;
+    var losses = 0;
+
+    document.onkeyup = function(event) {
+    var randNum = Math.floor(Math.random() * (2- 0 +1))+ 0;

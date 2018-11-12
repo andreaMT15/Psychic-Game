c5b89ff771dad7a0d6104953bbc704d663205005
Andrea Minhas <andreaminhas@Andreas-MacBook-Pro.local>
2018-11-11 18:53:20 -0600

    styling added and some js updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,50 @@
+//Global Variables
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var count = 9;
+var userWin = document.getElementById("wins");
+var userLoss = document.getElementById("losses");
 var userLetterInput = document.getElementById("guesses");
+var userAttempts = document.getElementById("remaining");
+
+var resetLetters = document.getElementById("guesses");
+
+//Game Logic Function
 
 document.onkeyup = function (event) {
-    var compLetter = letters[Math.floor(Math.random() * letters.length)];
     var userInput = event.key;
-    // userLetterInput.append(userInput, " ");
 
-
-    if ((userInput === compLetter) && (count>0)) {
+    if ((userInput === compLetter) && (count < 0)) {
         wins++;
-        count === 9;
+        count = 9;
         var compLetter = letters[Math.floor(Math.random() * letters.length)];
-        userLetterInput.append(userInput, " ");
-        var userWin = document.getElementById("losses");
+        userLetterInput.append(userInput, ", ");
         userWin.textContent = "Wins: " + wins;
-        console.log("guess letter: ", compLetter);
-        console.log("user input: ", userLetterInput);
+        alert("You Win!");
+            if (alert){
+                document.location.reload();
+            } 
     }
-    else if((userInput !== compLetter && count>0)){
-        count = count-1;
-        var userAttempts = document.getElementById("remaining");
+    else if ((userInput !== compLetter && count > 0)) {
+        count = count - 1;
         userAttempts.textContent = "Guesses Left: " + count;
-        userLetterInput.append(userInput, " ");
+        userLetterInput.append(userInput, ", ");
     }
 
-    else{
+    else {
         losses++;
-        count === 9;
+        count = 9;
         var compLetter = letters[Math.floor(Math.random() * letters.length)];
-        // userLetterInput.append(userInput, " ");
-        var userLoss = document.getElementById("losses");
         userLoss.textContent = "Losses: " + losses;
-        
+        alert("You Lose, try again.");
+            // if (alert){
+            //     document.location.reload();
+            // } 
+        resetLetters.textContent= "Your Guesses So Far: "
     }
+
 
 }
+
+//loss counter should increase once the player looses and the game should reset.

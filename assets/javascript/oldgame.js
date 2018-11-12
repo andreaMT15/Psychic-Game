cf47f77753b83fca667807f574c407e12f7efaf6
Andrea Minhas <andreaminhas@Andreas-MacBook-Pro.local>
2018-11-11 20:24:20 -0600

    corrected console log and fixed wins section. Also added a couple other js files for troubleshooting purposes.

--- assets/javascript/oldgame.js
+++ assets/javascript/oldgame.js
@@ -1,4 +1,3 @@
-//Global Variables
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
@@ -9,30 +8,28 @@ var userAttempts = document.getElementById("remaining");
 var userLetterInput = document.getElementById("guesses");
 var resetLetters = document.getElementById("guesses");
 
-//On Key Up Function
 document.onkeyup = function (event) {
     var compLetter = letters[Math.floor(Math.random() * letters.length)];
     var userInput = event.key;
     console.log("guess letter: ", compLetter);
     console.log("user input: ", userInput);
 
-// If else statements. If user letter = computer letter and count is greater than 0 increase wins and reset letters.
+
     if ((userInput === compLetter) && (count>0)) {
         wins++;
         count = 9;
         var compLetter = letters[Math.floor(Math.random() * letters.length)];
-        userLetterInput.append(userInput, ", ");
+        userLetterInput.append(userInput, " ");
         userWin.textContent = "Wins: " + wins;
         resetLetters.textContent= "Your Guesses So Far: ";
         
     }
-//If user letter does not = computer letter and cont is greater than 0 decrease count by 1 until it reaches 0. 
     else if((userInput !== compLetter && count>0)){
         count = count-1;
         userAttempts.textContent = "Guesses Left: " + count;
-        userLetterInput.append(userInput, ", ");
+        userLetterInput.append(userInput, " ");
     }
-// Once chances left = 0 user loses and loss count goes up by 1.
+
     else{
         losses++;
         count = 9;
--- assets/javascript/oldgame1.js
+++ assets/javascript/oldgame1.js
@@ -1,44 +1,39 @@
-//Global Variables
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var count = 9;
-var userWin = document.getElementById("wins");
-var userLoss = document.getElementById("losses");
-var userAttempts = document.getElementById("remaining");
 var userLetterInput = document.getElementById("guesses");
-var resetLetters = document.getElementById("guesses");
 
-//On Key Up Function
 document.onkeyup = function (event) {
     var compLetter = letters[Math.floor(Math.random() * letters.length)];
     var userInput = event.key;
-    console.log("guess letter: ", compLetter);
-    console.log("user input: ", userInput);
+    // userLetterInput.append(userInput, " ");
 
-// If else statements. If user letter = computer letter and count is greater than 0 increase wins and reset letters.
+
     if ((userInput === compLetter) && (count>0)) {
         wins++;
-        count = 9;
+        count === 9;
         var compLetter = letters[Math.floor(Math.random() * letters.length)];
-        userLetterInput.append(userInput, ", ");
+        userLetterInput.append(userInput, " ");
+        var userWin = document.getElementById("losses");
         userWin.textContent = "Wins: " + wins;
-        resetLetters.textContent= "Your Guesses So Far: ";
-        
+        console.log("guess letter: ", compLetter);
+        console.log("user input: ", userLetterInput);
     }
-//If user letter does not = computer letter and cont is greater than 0 decrease count by 1 until it reaches 0. 
     else if((userInput !== compLetter && count>0)){
         count = count-1;
+        var userAttempts = document.getElementById("remaining");
         userAttempts.textContent = "Guesses Left: " + count;
-        userLetterInput.append(userInput, ", ");
+        userLetterInput.append(userInput, " ");
     }
-// Once chances left = 0 user loses and loss count goes up by 1.
+
     else{
         losses++;
-        count = 9;
+        count === 9;
         var compLetter = letters[Math.floor(Math.random() * letters.length)];
+        // userLetterInput.append(userInput, " ");
+        var userLoss = document.getElementById("losses");
         userLoss.textContent = "Losses: " + losses;
-        resetLetters.textContent= "Your Guesses So Far: "
         
     }
 
